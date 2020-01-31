e370d8330193ee6f15a42836fb89dcd825679d1f
aesys <fcregs10@gmail.com>
2020-01-31 17:55:18 +0100

    Created a retrofitService 2

--- app/src/main/java/network/TestDto.kt
+++ app/src/main/java/network/TestDto.kt
@@ -1,7 +1,12 @@
 package network
 
+import com.google.gson.annotations.Expose
+
 data class TestDto(
+    @Expose
     val ms1: String?,
+    @Expose
     val msg2: String?,
+    @Expose
     val msg3: String?
     )
